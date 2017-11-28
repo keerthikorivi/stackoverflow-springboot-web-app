a9cac66e442bfd9dc8e3b64fdf73e4f1e4afa99d
Keerthi Korivi <keerthi.korivi@cerner.com>
2017-11-27 20:38:27 -0600

    main

---
+++ src/main/java/org/keerthi/StackOverFlowSpringBootWebApp.java
@@ -0,0 +1,12 @@
+package org.keerthi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverFlowSpringBootWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverFlowSpringBootWebApp.class, args);
+	}
+}
