16a7ee45026713dff505ba0ee1b269b9d1ad3309
Keerthi Korivi <keerthi.korivi@cerner.com>
2017-11-28 22:47:38 -0600

    add home

--- src/main/java/org/keerthi/Controller/HelloController.java
+++ src/main/java/org/keerthi/Controller/HelloController.java
@@ -10,5 +10,11 @@ public class HelloController {
 	public String sayHi() {
 		return "Hi!!!";
 	}
+	
+	@RequestMapping("/")
+	public String sayHi() {
+		return "Congrats you deployed you app to heroku!!!";
+	}
+
 
 }
